8d5bd01a48929e1ce70043e6aaeb4cf1fee5bb18
Rafael <berezanskyj16@gmail.com>
2025-03-24 23:01:22 -0300

    mudando config

--- config.php
+++ config.php
@@ -3,14 +3,14 @@
 define('APP_NAME',          'Adventure');
 define('APP_VERSION',          '1.0.0');
 
-// define('BASE_URL',           'http://localhost:8080/adventure/public/');
-define('BASE_URL',           'http://localhost/adventure/public/');
+define('BASE_URL',           'http://localhost:8080/adventure/public/');
+// define('BASE_URL',           'http://localhost/adventure/public/');
 
 //MYSQL
 define('MYSQL_SERVER',          'localhost');
 define('MYSQL_DATABASE',        'adventure');
-define('MYSQL_USER',            'root');
-define('MYSQL_PASS',            '');
+define('MYSQL_USER',            'adventure');
+define('MYSQL_PASS',            '3/zF5LB(oRN]M6vy');
 define('MYSQL_CHARSET',         'utf8mb4');
 
 //email
@@ -18,3 +18,30 @@ define('EMAIL_HOST',        'smtp.gmail.com');
 define('EMAIL_FROM',        'adventurecollecture@gmail.com');
 define('EMAIL_PASS',        'wasrwvrsoimcbers');
 define('EMAIL_PORT',        '587');
+
+
+
+
+
+
+
+//*USAR NO CURSO
+
+// define('APP_NAME',          'Adventure');
+// define('APP_VERSION',          '1.0.0');
+
+// define('BASE_URL',           'http://localhost:8080/adventure/public/');
+// // define('BASE_URL',           'http://localhost/adventure/public/');
+
+// //MYSQL
+// define('MYSQL_SERVER',          'localhost');
+// define('MYSQL_DATABASE',        'adventure');
+// define('MYSQL_USER',            'root');
+// define('MYSQL_PASS',            '');
+// define('MYSQL_CHARSET',         'utf8mb4');
+
+// //email
+// define('EMAIL_HOST',        'smtp.gmail.com');
+// define('EMAIL_FROM',        'adventurecollecture@gmail.com');
+// define('EMAIL_PASS',        'wasrwvrsoimcbers');
+// define('EMAIL_PORT',        '587');
